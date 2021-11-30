9f2e35dced3b74bbd51a6941ad74c2cfec921615
acerix <dylan@psilly.com>
2021-11-29 22:19:26 -0500

    kleanup

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 const Home: FunctionalComponent = () => {
   return (
     <section class="container">
-      <Helmet title="Beepody Belongs You" />
+      <Helmet title="Beepody" />
       <div class="px-4 pt-5 my-5 text-center border-bottom">
         <h1 class="h4 fw-bold">Beepody</h1>
         <div class="col-lg-6 mx-auto">
