ca10e3ac90c052fabf499410399ca9bbc4b03292
acerix <dylan@psilly.com>
2021-11-29 22:20:56 -0500

    Updates

--- route-home.chunk.578b8.esm.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{FDtd:function(e,t,c){"use strict";c.r(t);var o=c("hosL"),s=c("Hrl7");t.default=()=>Object(o.h)("section",{class:"container"},Object(o.h)(s.a,{title:"Beepody Belongs You"}),Object(o.h)("div",{class:"px-4 pt-5 my-5 text-center border-bottom"},Object(o.h)("h1",{class:"h4 fw-bold"},"Beepody"),Object(o.h)("div",{class:"col-lg-6 mx-auto"},Object(o.h)("p",{class:"lead mb-4"},"Beep."),Object(o.h)("p",{class:"mb-4"},"Bloop."))))}}]);
-//# sourceMappingURL=route-home.chunk.578b8.esm.js.map
